5a1aa9d6bfdc998b6c63daabc2914ffc775a4026
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-07-24 20:02:54 -0500

    added java

---
+++ howold.js
@@ -0,0 +1,30 @@
+console.log("linked");
+
+// 1. How many hours are in a year
+
+var hoursInADay = 24;
+var daysinAYear = 365;
+console.log(hoursInADay * daysinAYear);
+
+// 2. how many minutes are in a decade
+
+var minInaDay = hoursInADay * 60;
+var minYear = minInaDay * daysinAYear;
+var tenYear = minYear * 10;
+console.log("this is after 10 years", tenYear);
+
+
+// 3. How many second old are they
+
+var seconds = tenYear * 60;
+console.log("I am second", seconds);
+
+
+
+// 4. if they are older than some arbitrary number, console " I'm old", else "I'm young"
+
+if (seconds > 10000000000) {
+	console.log("I'm old");
+} else {
+	console.log("I'm young");
+}
