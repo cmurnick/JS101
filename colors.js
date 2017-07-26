a0f9548dc2d09e13fc6b1480b7a96d82adbb0d18
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-07-25 20:20:46 -0500

    for loop

--- colors.js
+++ colors.js
@@ -19,3 +19,34 @@ for (var  i = 0; i < array.length; i++) {
  // sort that array alphabetically
  // loop through the array and log to the console the statements: 
  		// "The color at"_index number_ "is"_color name_
+
+
+//    LOOPS
+// Write a for loop that uses a counter variable initialized at 5 and then increments it by 10 every time it executes. Use console.log() to output the value of the counter variable each time through the loop. Stop execution of the loop if the counter variable’s value is greater than 120.
+
+// Example output:
+
+// Current value is 5
+// Current value is 15
+// Current value is 25 (edited)
+
+
+
+for(var i = 5; i <= 120; i = i + 10) {
+	console.log("current value ", i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
